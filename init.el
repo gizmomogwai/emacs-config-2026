ef55d4b299f4258be10dd87d87f861dcf5c1498a
Christian Koestlin <christian.koestlin@gmail.com>
2026-02-09 09:22:29 +0100

    Fix eglot-x git remote

--- init.el
+++ init.el
@@ -312,7 +312,7 @@ point reaches the beginning or end of the buffer, stop there."
   )
 
 (use-package eglot-x
-  :ensure (eglot-x :type git :host github :repo "nemethf/eglot-x.git")
+  :ensure (eglot-x :type git :host github :repo "nemethf/eglot-x")
   :after (eglot)
   :config (eglot-x-setup)
   )
